4a2c32c3ea5c534758c16b9527ad3a7239942d3b
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-03-13 02:20:06 -0400

    finish storing game data

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,27 @@
-// api endpoint:
-var queryURL = 'https://restcountries.eu/rest/v2/all?fields=name;capital;languages'
+// api endpoint (include languages?):
+var queryURL = 'https://restcountries.eu/rest/v2/all?fields=name;capital;population;region;area;'
 //code to run on data return
 function init(result) {
-    var clueType = ['name', 'languages', 'capital'];
+    var clueTypes = ['population', 'capital','region','area'];
+    var clueType = clueTypes[random(clueTypes.length)];
     var countryIndex = random(result.length);
     var country = result[countryIndex];
-    var languages = result[countryIndex].languages;
-    var languagesArr = [];
-    languages.forEach(function(language) {
-  		languagesArr.push(language.name);
-	});
-	console.log(languagesArr);
+    // var languages = result[countryIndex].languages;
+    // var languagesArr = [];
+	// languages.forEach(function(language) {
+	// 		languagesArr.push(language.name);
+	// });
+	var clue = `What is the ${clueType} of ${country.name}`;
+	console.log(clue);
+	console.log('cluetype', clueType);
+	var answer = country[clueType];
+	console.log('answer', answer);
+	console.log(country);
+	var wrongAnswer = function(){
+		return result[random(result.length)][clueType]
+	}
+	var wrongAnswers =[wrongAnswer(), wrongAnswer(), wrongAnswer()];
+	console.log(wrongAnswers)
 }
 
 //execute when server responds
