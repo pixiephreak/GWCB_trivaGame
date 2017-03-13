a8dc0720504a776184c9f8a69219c62e55d0f74f
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-03-13 15:49:47 -0400

    create fall back mode for api failure

--- assets/app.js
+++ assets/app.js
@@ -1,32 +1,13 @@
+
 // api endpoint (include languages?):
 var queryURL = 'https://restcountries.eu/rest/v2/all?fields=name;capital;population;region;area;'
 //code to run on data return
 function init(result) {
-    var clueTypes = ['population', 'capital','region','area'];
-    var clueType = clueTypes[random(clueTypes.length)];
-    var countryIndex = random(result.length);
-    var country = result[countryIndex];
-    // var languages = result[countryIndex].languages;
-    // var languagesArr = [];
-	// languages.forEach(function(language) {
-	// 		languagesArr.push(language.name);
-	// });
-	var question = `What is the ${clueType} of ${country.name}`;
-	console.log(question)
-	$('#question').html(question);
-	console.log('cluetype', clueType);
-	var answer = country[clueType];
-	console.log('answer', answer);
-	$('#choices').append(`<li>${answer.toLocaleString()}</li>`);
-	console.log(country);
-	var wrongAnswer = function(){
-		return result[random(result.length)][clueType]
-	}
-	//insert excpetion for entries that lack a property
-	var wrongAnswers =[wrongAnswer(), wrongAnswer(), wrongAnswer()];
-	console.log(wrongAnswers)
-	wrongAnswers.forEach(function(answer){$('#choices').append(`<li>${answer.toLocaleString('en')}</li>`)}
-);
+	game()
+}
+
+function onFail(){
+	game()
 }
 
 //execute when server responds
@@ -37,7 +18,8 @@ function apiCall(callback) {
     $.ajax({
              url: queryURL,
              method: 'GET',
-             success: callback
+             success: callback,
+             error: onFail()
            })
 }
 
@@ -45,3 +27,35 @@ function random(length){
 		num = Math.floor((Math.random() * length) + 0);
 		return num;
 	}
+
+setTimeout(function(){
+        console.log('timer')
+      },15000)
+
+function game(){
+	    var clueTypes = ['population', 'capital','region','area'];
+	    var clueType = clueTypes[random(clueTypes.length)];
+	    var countryIndex = random(result.length);
+	    var country = result[countryIndex];
+	    // var languages = result[countryIndex].languages;
+	    // var languagesArr = [];
+		// languages.forEach(function(language) {
+		// 		languagesArr.push(language.name);
+		// });
+		var question = `What is the ${clueType} of ${country.name}`;
+		console.log(question)
+		$('#question').html(question);
+		console.log('cluetype', clueType);
+		var answer = country[clueType];
+		console.log('answer', answer);
+		$('#choices').append(`<li>${answer.toLocaleString()}</li>`);
+		console.log(country);
+		var wrongAnswer = function(){
+			return result[random(result.length)][clueType]
+		}
+		//insert excpetion for entries that lack a property
+		var wrongAnswers =[wrongAnswer(), wrongAnswer(), wrongAnswer()];
+		console.log(wrongAnswers)
+		wrongAnswers.forEach(function(answer){$('#choices').append(`<li>${answer.toLocaleString('en')}</li>`)}
+	);
+}
