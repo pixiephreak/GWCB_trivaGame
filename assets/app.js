3aabe5210c6178dc1161cd9306982b30cdc544d8
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-03-14 17:54:51 -0400

    write startGameAgain()

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,14 @@
 
 	// api endpoint (include languages?):
 	var queryURL = 'https://restcountries.eu/rest/v2/all?fields=name;capital;population;region;area;';
-	var gameLength = 2;
-	var timerDuration = 12*1000;
-	var time = 0;
+	var gameLength = 4;
+	var timerDuration = 22*1000;
+	var time;
 	var answer;
 	var score = 0;
 	var intervalId;
 	var stopwatchIntervalId;
+	var correctIntervalId;
 	var counter=0;
 	//code to run on data return
 	var game = {
@@ -17,22 +18,38 @@
 				//figure out how to start stopwatch and hide 'page loading' when api comes back (is it in ajax call?)
 				stopwatch.start();
 				$('#loadingDiv').html('Page loading...');
-				setTimeout(game.removeLoader(), 1000);
+				setTimeout(game.removeLoader(), 100);
 				$('#start').toggleClass('hidden');
 				$('#loading').html("questions loading");
-				//why is there a ten second delay before game.showQuestion fires?
-				intervalId = setInterval(game.showQuestion,timerDuration);
+				//make first question load instantly
+				game.showQuestion();
+				//load reamaining questions at interval
+				game.startGame();
 				$('#choices').off('click', '.choice').on('click', '.choice', function(){
 					if(($(this).html()) == answer){
-						score++;
-						$('#result').html('Correct!');
-						$('#question').empty();
-						$('#choices').empty();
-
+						game.correct();
+						game.thisInterval(game.startGameAgain, 5000, 1)
+					}else{
+						console.log('click wrong', $(this));
 					}
+
+
 				});
 			});
 
+		},
+		startGame: function(){
+			intervalId = setInterval(game.showQuestion,timerDuration-1);
+		},
+		startGameAgain: function(){
+			gameLength = gameLength-counter;
+			stopwatch.reset();
+			stopwatch.start();
+			game.showQuestion();
+			game.startGame();
+		},
+		stopGame: function(){
+			clearInterval(intervalId);
 		},
 		showQuestion: function(){
 			counter++;
@@ -41,9 +58,9 @@
 			$('#choices').empty();
 			$('#result').empty();
 			var clueTypes = ['population', 'capital','region','area'];
-		    var clueType = clueTypes[game.random(clueTypes.length)];
-		    var countryIndex = game.random(result.length);
-		    var country = result[countryIndex];
+			var clueType = clueTypes[game.random(clueTypes.length)];
+			var countryIndex = game.random(result.length);
+			var country = result[countryIndex];
 		    // var languages = result[countryIndex].languages;
 		    // var languagesArr = [];
 			// languages.forEach(function(language) {
@@ -52,13 +69,13 @@
 			var question = `What is the ${clueType} of ${country.name}`;
 			console.log(question);
 			$('#question').html(question);
-			answer = country[clueType];
+			answer = country[clueType].toString();
 			console.log('answer', answer);
 			if(typeof answer != 'undefined'){
 				$('#choices').append(`<li class="choice">${answer.toLocaleString()}</li>`);
-				}else{
+			}else{
 				$('#choices').append(`<li class="choice">Freebie.We don't know.</li>`);
-				}
+			}
 
 			wrongAnswers = [];
 			function wrongChoices(){
@@ -68,101 +85,124 @@
 						};
 						for(let i=0; i<3; i++){
 							var thisWrongAnswer = wrongAnswer();
-								if(wrongAnswers.indexOf(thisWrongAnswer) === -1 && thisWrongAnswer != answer){
-									if(typeof thisWrongAnswer != 'undefined')
+							if(wrongAnswers.indexOf(thisWrongAnswer) === -1 && thisWrongAnswer != answer){
+								if(typeof thisWrongAnswer != 'undefined')
 									wrongAnswers.push(thisWrongAnswer);
-								}else{
-									wrongAnswer.push("I really don't know.")
-								}
+							}else{
+								wrongAnswers.push("I really don't know.")
+							}
 							console.log(wrongAnswers);
 						}
 						wrongAnswers.forEach(function(answer){
 							$('#choices').append(`<li class="choice">${answer.toLocaleString('en')}</li>`);
 						});
 
-				}
-			wrongChoices();
-			if(counter > gameLength){
-				clearInterval(intervalId);
-				clearInterval(stopwatchIntervalId);
-				counter = 0;
-				$('#timer').empty();
-				$('#question').empty();
-				$('#choices').empty();
-				$('#start').toggleClass('hidden');
-			}
-		},
-		removeLoader: function(){
+					}
+					wrongChoices();
+					game.checkGameLangth();
+				},
+				correct: function(){
+					score++;
+					$('#score').html(`Score: ${score}`)
+					$('#result').html('Correct!');
+					$('#question').empty();
+					$('#choices').empty();
+					$('#question').html('Please stand by for the next question.')
+					stopwatch.pause();
+					game.stopGame();
+				},
+				checkGameLangth: function(){
+					if(counter > gameLength){
+						game.stopGame();
+						clearInterval(stopwatchIntervalId);
+						counter = 0;
+						$('#timer').empty();
+						$('#question').empty();
+						$('#choices').empty();
+						$('#start').toggleClass('hidden');
+					}
+				},
+				thisInterval: function(callback, delay, repetitions) {
+					var x = 0;
+					var intervalID = window.setInterval(function () {
+						callback();
+						if (++x === repetitions) {
+							window.clearInterval(intervalID);
+						}
+					}, delay);
+				},
+				removeLoader: function(){
 			// fadeOut complete. Remove the loading div
 			$( "#loadingDiv" ).fadeOut(3000, function() {
       			 $( "#loadingDiv" ).remove(); //makes page more lightweight
-  			});
-  		},
+      			});
+		},
 		random: function(length){
 			num = Math.floor((Math.random() * length) + 0);
 			return num;
 		}
 
-	}
+	};
 
 	var stopwatch = {
-
-	  time: 0,
-	  count: function() {
-	        seconds = stopwatch.time++
-	        $('#timer').html(stopwatch.timeConverter(seconds));
-	  },
-	  start: function(){
-	  	stopwatchIntervalId = setInterval(stopwatch.count,1000);
-	  },
-	  timeConverter: function(t) {
+		count: function() {
+			seconds = stopwatch.time--;
+			$('#timer').html(stopwatch.timeConverter(seconds));
+		},
+		start: function(){
+			stopwatchIntervalId = setInterval(stopwatch.count,1000);
+		},
+		pause: function(){
+			clearInterval(stopwatchIntervalId);
+		},
+		timeConverter: function(t) {
 
 		    //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
 		    var minutes = Math.floor(t / 60);
 		    var seconds = t - (minutes * 60);
 
 		    if (seconds < 10) {
-		      seconds = "0" + seconds;
+		    	seconds = "0" + seconds;
 		    }
 
 		    if (minutes === 0) {
-		      minutes = "00";
+		    	minutes = "00";
 		    }
 
 		    else if (minutes < 10) {
-		      minutes = "0" + minutes;
+		    	minutes = "0" + minutes;
 		    }
 
 		    return minutes + ":" + seconds;
-		  	},
-		  	reset: function() {
+		},
+		reset: function() {
 
-		  	  stopwatch.time = 0;
+			stopwatch.time = 20;
 		  	  //  TODO: Change the "display" div to "00:00."
-		  	  $('#timer').html('00:00');
+		  	  $('#timer').html('00:20');
 
 		  	}
-		}
+		  };
 
-	function init(result) {
-		game.init();
-	}
+		  function init(result) {
+		  	game.init();
+		  }
 
-	function onFail(){
-		game.init();
-	}
+		  function onFail(){
+		  	game.init();
+		  }
 
 	//execute when server responds
 	apiCall(init);
 
 	// apiCall itself is defined as follows:
 	function apiCall(callback) {
-	    $.ajax({
-	             url: queryURL,
-	             method: 'GET',
-	             success: callback,
-	             error: onFail()
-	           });
+		$.ajax({
+			url: queryURL,
+			method: 'GET',
+			success: callback,
+			error: onFail()
+		});
 	}
 
 
