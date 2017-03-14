959d2238b4200a719e20db69abc46c413a9c9506
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-03-14 12:42:42 -0400

    debug wrongAnswers() and add page loading on start click

--- assets/app.js
+++ assets/app.js
@@ -4,27 +4,33 @@
 	var gameLength = 5;
 	var answer;
 	var score = 0;
+	var count = 0;
+	var intervalId;
 	//code to run on data return
 	var game = {
 		init: function(){
-			var startGame;
-			$('#start').on('click', function(){
+
+			$('#start').off('click').on('click', function(){
+				$('#loadingDiv').html('Page loading...')
+				setTimeout(game.removeLoader(), 1000);
 				$('#start').toggleClass('hidden');
-				$('#choices').empty();
-				game.showQuestion();
-				var intervalID=game.thisInterval(function () {
-				    game.showNextQuestion();
-				}, 5000, gameLength-1);
+				$('#loading').html("questions loading");
+				game.questions();
+
 			})
+
 			$('#choices').off('click', '.choice').on('click', '.choice', function(){
 				if(($(this).html()) == answer){
 					score++;
 					$('#result').html('Correct!');
-					clearInterval(intervalID);
 				}
 			})
 		},
 		showQuestion: function(){
+			count++;
+			console.log(count);
+			console.log(gameLength);
+			$('#choices').empty();
 			var clueTypes = ['population', 'capital','region','area'];
 		    var clueType = clueTypes[game.random(clueTypes.length)];
 		    var countryIndex = game.random(result.length);
@@ -42,45 +48,48 @@
 			console.log('answer', answer);
 			if(typeof answer != 'undefined'){
 				$('#choices').append(`<li class="choice">${answer.toLocaleString()}</li>`);
-				}else{
-					game.showQuestion();
 				}
 			console.log(country);
-			var wrongAnswers =[];
+			wrongAnswers = [];
 			function wrongChoices(){
-					//insert excpetion for entries that lack a property
-					var wrongAnswer = function(){
-						return result[game.random(result.length)][clueType];
-					}
-					for(let i=0; i<3; i++){
-						var thisWrongAnswer = wrongAnswer();
-							if(wrongAnswers.indexOf(thisWrongAnswer) === -1 || typeof thisWrongAnswer != 'undefined'){
-								console.log('adding', thisWrongAnswer);
-								wrongAnswers.push(thisWrongAnswer);
-							}
-					}
-					wrongAnswers.forEach(function(answer){
-						$('#choices').append(`<li class="choice">${answer.toLocaleString('en')}</li>`)}
-				);
-			}
+						//insert excpetion for entries that lack a property
+						var wrongAnswer = function(){
+							return result[game.random(result.length)][clueType];
+						}
+						for(let i=0; i<3; i++){
+							var thisWrongAnswer = wrongAnswer();
+							// typeof thisWrongAnswer != 'undefined'
+							console.log(thisWrongAnswer, wrongAnswers.indexOf(thisWrongAnswer), typeof thisWrongAnswer);
+								if(wrongAnswers.indexOf(thisWrongAnswer) === -1 && thisWrongAnswer != answer){
+									if(typeof thisWrongAnswer != 'undefined')
+									wrongAnswers.push(thisWrongAnswer);
+								}else{
+									var thisWrongAnswer = wrongAnswer();
+									if(wrongAnswers.indexOf(thisWrongAnswer) === -1){
+										if(typeof thisWrongAnswer != 'undefined')
+										wrongAnswers.push(thisWrongAnswer);
+									}
+								}
+							console.log(wrongAnswers);
+						}
+						wrongAnswers.forEach(function(answer){
+							$('#choices').append(`<li class="choice">${answer.toLocaleString('en')}</li>`);
+						})
+
+				}
 			wrongChoices();
+		},
+		questions: function(){
+
+		intervalId = setInterval(game.showQuestion,5*1000);
 
 		},
-		thisInterval: function(callback, delay, repetitions) {
-	    var x = 0;
-	    var intervalID = window.setInterval(function () {
-
-	       callback();
-
-		       if (++x === repetitions) {
-		           	window.clearInterval(intervalID);
-		       	}
-	    	}, delay);
-		},
-		showNextQuestion: function(){
-			  $('#choices').empty();
-			  game.showQuestion();
-		},
+		removeLoader: function(){
+			// fadeOut complete. Remove the loading div
+			$( "#loadingDiv" ).fadeOut(3000, function() {
+      			 $( "#loadingDiv" ).remove(); //makes page more lightweight
+  			});
+  		},
 		random: function(length){
 			num = Math.floor((Math.random() * length) + 0);
 			return num;
@@ -108,5 +117,17 @@
 	           })
 	}
 
+
+		// thisInterval: function(callback, delay, repetitions) {
+	 //    var x = 0;
+	 //    var intervalID = window.setInterval(function () {
+
+	 //       callback();
+
+		//        if (++x === repetitions) {
+		//            	window.clearInterval(intervalID);
+		//        	}
+	 //    	}, delay);
+		// },
 
 
